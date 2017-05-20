67ff59737846f241e8e6d25bd36a2f4fe0e19630
Yevgen Bushuyev <yevgen.bushuyev@gmail.com>
2017-05-20 12:12:27 +0200

    block moving

--- src/main/java/com/test/mod1/MyBlock.java
+++ src/main/java/com/test/mod1/MyBlock.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -20,13 +21,33 @@ class MyBlock extends Block {
   }
 
   @Override
+  public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+    System.out.println("onBlockClicked");
+  }
+
+
+
+  @Override
   public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
     System.out.println("onEntityWalk "+pos.toString());
-    pos.add(10, 10, 100);
 
-    worldIn.setBlockState(pos, this.getDefaultState());
-    worldIn.updateEntity(entityIn);
+    BlockPos p = entityIn.getPosition();
+    entityIn.setPosition(p.getX()+2, p.getY()+2, p.getZ());
+
+//    worldIn.setBlockToAir(pos);
+
+//    pos = pos.add(10, 10, 100);
+
+//    worldIn.setBlockState(pos, this.getDefaultState(), 2);
+
+
+//    worldIn.set
+
+
+//    worldIn.tick();
 
 //    worldIn.setBlockToAir(pos);
   }
+
+
 }
